2512623b8af67e9f8e82a049e9eb83a712fa20ec
Tom.Tom <tomsemail@somedomain.com>
2022-10-02 23:28:01 -0400

    More updates.

--- auto-net.py
+++ auto-net.py
@@ -91,16 +91,16 @@ class AutoNet:
 
 		for a, b in raw.get('scan').items():
 			if str(b['status']['state']) == 'down' and str(b['status']['reason']) == 'no-response':
-			try:
-				self.logmsg("ipv4: %s" % (str(b['addresses']['ipv4'] )))
-				self.logmsg("state:  %s" % (str(b['status']['state']  )))
+				try:
+					self.logmsg("ipv4: %s" % (str(b['addresses']['ipv4'] )))
+					self.logmsg("state:  %s" % (str(b['status']['state']  )))
 
-				self.finlst.append([str(b['addresses']['ipv4']), str(b['status']['state'])])
-				self.logmsg("a, b: %s %s" % (str(a), str(b)))
-			except Exception as listexc:
-				self.logmsg("Error inserting element: %s %s" % (a, b))
-				self.logmsg("Exception Encountered: ", listexc)
-				continue
+					self.finlst.append([str(b['addresses']['ipv4']), str(b['status']['state'])])
+					self.logmsg("a, b: %s %s" % (str(a), str(b)))
+				except Exception as listexc:
+					self.logmsg("Error inserting element: %s %s" % (a, b))
+					self.logmsg("Exception Encountered: ", listexc)
+					continue
 
 		self.logmsg("self.finlst: %s" % (self.finlst))
 
